ab428e78f7c755dfaf36cbece910f3ccca7fee2d
sanchaykumar <sanchaykumar9@gmail.com>
2024-08-23 23:35:01 +0530

    add autocmds for term bg color

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -2,6 +2,22 @@ local function augroup(name)
   return vim.api.nvim_create_augroup(name, { clear = true })
 end
 
+vim.api.nvim_create_autocmd({ "UIEnter", "ColorScheme" }, {
+  callback = function()
+    local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
+    if not normal.bg then
+      return
+    end
+    io.write(string.format("\027]11;#%06x\027\\", normal.bg))
+  end,
+})
+
+vim.api.nvim_create_autocmd("UILeave", {
+  callback = function()
+    io.write "\027]111\027\\"
+  end,
+})
+
 vim.api.nvim_create_autocmd({ "TermOpen", "TermEnter" }, {
   group = augroup "term_enter",
   callback = function()
