dedbe1ffcc9b5959bb10cbbc53d58b4a17190763
sanchaykumar <sanchaykumar9@gmail.com>
2024-09-13 20:02:12 +0530

    update treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -33,6 +33,7 @@ return {
           "zathurarc",
           "hyprlang",
           "json",
+          "yaml",
           "gitcommit",
           "git_config",
           "rasi",
@@ -41,13 +42,14 @@ return {
           "gosum",
           "gowork",
           "http",
+          "terraform",
         },
         ignore_install = { "javascript" },
         highlight = {
           enable = true,
           disable = function(lang, buf)
             local max_filesize = 100 * 1024 -- 100 KB
-            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
             if ok and stats and stats.size > max_filesize then
               return true
             end
