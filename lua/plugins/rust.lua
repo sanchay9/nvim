45f5c48a01c1fd3fccc6f8335b37311cdadfde5e
sanchaykumar <sanchaykumar9@gmail.com>
2024-11-28 21:40:49 +0530

    add rust plugins

---
+++ lua/plugins/rust.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^5", -- Recommended
+    lazy = false, -- This plugin is already lazy
+    config = function()
+      vim.g.rustaceanvim = {
+        -- Plugin configuration
+        tools = {},
+        -- LSP configuration
+        server = {
+          on_attach = function(client, bufnr)
+            -- you can also put keymaps in here
+          end,
+          default_settings = {
+            -- rust-analyzer language server configuration
+            ["rust-analyzer"] = {},
+          },
+        },
+        dap = {},
+      }
+    end,
+  },
+}
