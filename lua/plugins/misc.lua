592c295712cac8a3cd437fb21918e17315dee520
sanchaykumar <sanchaykumar9@gmail.com>
2024-11-28 21:52:13 +0530

    add mini animate

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -23,4 +23,35 @@ return {
     },
     dependencies = { "NvChad/volt" },
   },
+  {
+    "echasnovski/mini.animate",
+    event = "VeryLazy",
+    opts = function()
+      -- don't use animate when scrolling with the mouse
+      local mouse_scrolled = false
+      for _, scroll in ipairs { "Up", "Down" } do
+        local key = "<ScrollWheel" .. scroll .. ">"
+        vim.keymap.set({ "", "i" }, key, function()
+          mouse_scrolled = true
+          return key
+        end, { expr = true })
+      end
+
+      local animate = require "mini.animate"
+      return {
+        scroll = {
+          timing = animate.gen_timing.linear { duration = 150, unit = "total" },
+          subscroll = animate.gen_subscroll.equal {
+            predicate = function(total_scroll)
+              if mouse_scrolled then
+                mouse_scrolled = false
+                return false
+              end
+              return total_scroll > 1
+            end,
+          },
+        },
+      }
+    end,
+  },
 }
