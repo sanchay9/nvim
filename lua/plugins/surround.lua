f62e0f1d0d69fa809ce41e8d50f175dcada42211
sanchaykumar <sanchaykumar9@gmail.com>
2024-12-17 00:54:31 +0530

    update surround mappings and enable

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,31 +1,23 @@
 return {
   "echasnovski/mini.surround",
-  enabled = false,
-  keys = function(_, keys)
-    -- Populate the keys based on the user's options
-    local mappings = {
-      { "sa", desc = "Add Surrounding", mode = { "n", "v" } },
-      { "sd", desc = "Delete Surrounding" },
-      { "sf", desc = "Find Right Surrounding" },
-      { "sF", desc = "Find Left Surrounding" },
-      { "sh", desc = "Highlight Surrounding" },
-      { "sr", desc = "Replace Surrounding" },
-      { "sn", desc = "Update `MiniSurround.config.n_lines`" },
-    }
-    mappings = vim.tbl_filter(function(m)
-      return m[1] and #m[1] > 0
-    end, mappings)
-    return vim.list_extend(mappings, keys)
-  end,
+  keys = {
+    { "gsa", desc = "Add Surrounding", mode = { "n", "v" } },
+    { "gsd", desc = "Delete Surrounding" },
+    { "gsf", desc = "Find Right Surrounding" },
+    { "gsF", desc = "Find Left Surrounding" },
+    { "gsh", desc = "Highlight Surrounding" },
+    { "gsr", desc = "Replace Surrounding" },
+    { "gsn", desc = "Update `MiniSurround.config.n_lines`" },
+  },
   opts = {
     mappings = {
-      add = "sa", -- Add surrounding in Normal and Visual modes
-      delete = "sd", -- Delete surrounding
-      find = "sf", -- Find surrounding (to the right)
-      find_left = "sF", -- Find surrounding (to the left)
-      highlight = "sh", -- Highlight surrounding
-      replace = "sr", -- Replace surrounding
-      update_n_lines = "sn", -- Update `n_lines`
+      add = "gsa",
+      delete = "gsd",
+      find = "gsf",
+      find_left = "gsF",
+      highlight = "gsh",
+      replace = "gsr",
+      update_n_lines = "gsn",
     },
   },
 }
