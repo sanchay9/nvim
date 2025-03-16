97c405697f798d733f60bf2f0dd68fef5e036b4a
sanchaykumar <sanchaykumar9@gmail.com>
2025-03-16 17:58:18 +0530

    update autocommands

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,25 +1,5 @@
-local function augroup(name)
-  return vim.api.nvim_create_augroup(name, { clear = true })
-end
-
-vim.api.nvim_create_autocmd({ "UIEnter", "ColorScheme" }, {
-  callback = function()
-    local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
-    if not normal.bg then
-      return
-    end
-    io.write(string.format("\027]11;#%06x\027\\", normal.bg))
-  end,
-})
-
-vim.api.nvim_create_autocmd("UILeave", {
-  callback = function()
-    io.write "\027]111\027\\"
-  end,
-})
-
 vim.api.nvim_create_autocmd({ "TermOpen", "TermEnter" }, {
-  group = augroup "term_enter",
+  group = vim.api.nvim_create_augroup("term_enter", { clear = true }),
   callback = function()
     -- vim.cmd.startinsert()
     vim.opt_local.nu = false
@@ -27,8 +7,8 @@ vim.api.nvim_create_autocmd({ "TermOpen", "TermEnter" }, {
 })
 
 vim.api.nvim_create_autocmd("BufReadPost", {
-  desc = "remember last position",
-  group = augroup "last_loc",
+  desc = "goto last position when opening buffer",
+  group = vim.api.nvim_create_augroup("last_position", { clear = true }),
   callback = function(event)
     local exclude = { "gitcommit" }
     local buf = event.buf
@@ -46,12 +26,13 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 
 vim.api.nvim_create_autocmd({ "VimEnter" }, {
   desc = "open oil on directory, alpha on no args",
-  group = augroup "vim enter",
+  group = vim.api.nvim_create_augroup("vim enter", { clear = true }),
   callback = function(data)
     if vim.fn.isdirectory(data.file) == 1 then
       vim.cmd.cd(data.file)
       require("oil").open(data.file)
-      return
+      -- local fzf_lua = require "fzf-lua"
+      -- fzf_lua.files { cwd = data.file }
     end
 
     if vim.fn.argc() > 0 or vim.fn.line2byte "$" ~= -1 or not vim.o.modifiable then
@@ -69,7 +50,7 @@ vim.api.nvim_create_autocmd({ "VimEnter" }, {
 
 vim.api.nvim_create_autocmd({ "FocusGained", "TermLeave", "TermClose" }, {
   desc = "reload files on events",
-  group = augroup "checktime",
+  group = vim.api.nvim_create_augroup("checktime", { clear = true }),
   callback = function()
     if vim.o.buftype ~= "nofile" then
       vim.cmd "checktime"
@@ -77,13 +58,51 @@ vim.api.nvim_create_autocmd({ "FocusGained", "TermLeave", "TermClose" }, {
   end,
 })
 
--- Use relative & absolute line numbers in 'n' & 'i' modes respectively
--- vim.cmd [[ au InsertEnter * set norelativenumber ]]
--- vim.cmd [[ au InsertLeave * set relativenumber ]]
+vim.api.nvim_create_autocmd("FileType", {
+  desc = "make it easier to close man-files when opened inline",
+  group = vim.api.nvim_create_augroup("man_unlisted", { clear = true }),
+  pattern = { "man" },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  desc = "set wrap for text files",
+  group = vim.api.nvim_create_augroup("wrap", { clear = true }),
+  pattern = { "text", "plaintex", "http", "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  desc = "auto create parent dir if doesn't exist on saving a file",
+  group = vim.api.nvim_create_augroup("auto_create_dir", { clear = true }),
+  callback = function(event)
+    if event.match:match "^%w%w+:[\\/][\\/]" then
+      return
+    end
+    local file = vim.uv.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "InsertLeave", "InsertEnter" }, {
+  desc = "toggle relativenumber on mode change",
+  group = vim.api.nvim_create_augroup("toggle_relativenumber", { clear = true }),
+  callback = function(event)
+    if event.event == "InsertLeave" then
+      vim.opt_local.relativenumber = true
+    else
+      vim.opt_local.relativenumber = false
+    end
+  end,
+})
 
 vim.api.nvim_create_autocmd("FileType", {
   desc = "close ft with q",
-  group = augroup "close_with_q",
+  group = vim.api.nvim_create_augroup("close_with_q", { clear = true }),
   pattern = {
     "gitsigns-blame",
     "PlenaryTestPopup",
@@ -95,7 +114,6 @@ vim.api.nvim_create_autocmd("FileType", {
     "man",
     "checkhealth",
     "lspinfo",
-    "spectre_panel",
     "dap-repl",
     "dap-float",
     "fugitive",
@@ -105,24 +123,34 @@ vim.api.nvim_create_autocmd("FileType", {
     "neotest-summary",
     "neotest-output",
     "neotest-output-panel",
+    "grug-far",
   },
   callback = function(event)
     vim.bo[event.buf].buflisted = false
-    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+    vim.schedule(function()
+      vim.keymap.set("n", "q", function()
+        vim.cmd "close"
+        pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+      end, {
+        buffer = event.buf,
+        silent = true,
+        desc = "Quit buffer",
+      })
+    end)
   end,
 })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "highlight on yank",
-  group = augroup "highlight_yank",
+  group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
   callback = function()
-    vim.highlight.on_yank { higroup = "Cursor", timeout = 200 }
+    vim.hl.on_yank { higroup = "Cursor", timeout = 200 }
   end,
 })
 
 vim.api.nvim_create_autocmd({ "VimResized" }, {
   desc = "resize splits if window got resized",
-  group = augroup "resize_splits",
+  group = vim.api.nvim_create_augroup("resize_splits", { clear = true }),
   callback = function()
     local current_tab = vim.fn.tabpagenr()
     vim.cmd "tabdo wincmd ="
