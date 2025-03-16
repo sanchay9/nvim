c0efcc9168456046d589aa7d1a73190bfa27c2ea
sanchaykumar <sanchaykumar9@gmail.com>
2025-03-16 19:34:57 +0530

    replace alpha with snacks dashboard

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -29,6 +29,60 @@ return {
         only_current = true,
       },
     },
+    dashboard = {
+      preset = {
+        header = require("banners")["vim"],
+        ---@type snacks.dashboard.Item[]
+        keys = {
+          { icon = " ", key = "w", desc = "notes", action = ":cd ~/docs/notes | e index.md" },
+          { icon = " ", key = "e", desc = "work notes", action = ":cd ~/docs/worknotes | e index.md" },
+          {
+            icon = "",
+            key = "c",
+            desc = "workdirs",
+            action = function()
+              local fzf_lua = require "fzf-lua"
+              fzf_lua.files {
+                cmd = "fd --min-depth 1 --max-depth 1 --type d . ~ ~/work ~/work/learngo ~/dev ~/.config 2>/dev/null",
+                actions = {
+                  ["enter"] = function(selected)
+                    if not selected[1] then
+                      return
+                    end
+                    local newcwd = string.sub(selected[1], 8)
+                    fzf_lua.files {
+                      cwd = newcwd,
+                      actions = {
+                        ["enter"] = function(selecte, opts)
+                          require("fzf-lua").actions.file_edit(selecte, opts)
+                          vim.cmd.cd(newcwd)
+                        end,
+                      },
+                    }
+                  end,
+                },
+              }
+            end,
+          },
+
+          -- { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          -- { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          -- { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          -- { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          -- { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          -- { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          -- { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+          -- { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+          { icon = " ", key = "q", desc = "quit", action = ":qa" },
+        },
+      },
+      sections = {
+        { section = "header" },
+        { section = "keys", gap = 1, padding = 1 },
+        { section = "startup" },
+        -- vim.system({ "curl", "https://vtip.43z.one" }):wait().stdout,
+      },
+    },
   },
   -- stylua: ignore
   keys = {
@@ -36,5 +90,6 @@ return {
     { "[[", function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" }, },
     { "<leader>gx", function() Snacks.gitbrowse() end, desc = "Git Browse", },
     { "<leader>`", function() Snacks.notifier.show_history() end, desc = "Notification History" },
+    { "<leader>,", function() Snacks.dashboard() end, desc = "Dashboard" },
   },
 }
