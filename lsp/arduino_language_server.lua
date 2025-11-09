768a273f46cc612737296f7c6393b47a68cc9e4f
sanchaykumar <sanchaykumar9@gmail.com>
2025-11-10 01:32:03 +0530

    add arduino ls confs

--- lsp/arduino_language_server.lua
+++ lsp/arduino_language_server.lua
@@ -1,7 +1,11 @@
 ---@type vim.lsp.Config
 return {
   filetypes = { "arduino" },
-  root_markers = { ".git" },
+  root_dir = function(bufnr, on_dir)
+    on_dir(vim.fs.root(bufnr, function(name)
+      return name:match "%.ino$" ~= nil
+    end) or vim.fs.dirname(vim.api.nvim_buf_get_name(bufnr)))
+  end,
   cmd = {
     "arduino-language-server",
   },
